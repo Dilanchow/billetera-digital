5f62f8b9913aafefbddb0992c8343dfac8ad7c6f
Dilanchow <127273039+Dilanchow@users.noreply.github.com>
2024-11-21 17:32:21 -0500

    Update Dashboard.tsx

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button } from "@/components/ui/button"
+import { Button } from "../components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 
 export const Dashboard = ({ user, onLogout, onNavigate }) => {
